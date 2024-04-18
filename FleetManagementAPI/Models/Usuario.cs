eff2e37c543439b4b7bf18ced1bc9a5942c5f649
MarFueGit <maricelafuentes100@gmail.com>
2024-04-17 20:38:19 -0600

    se instalo paquetes con NuGet y tambien hize el primer modelo usuario de ejemplo

---
+++ FleetManagementAPI/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FleetManagementAPI.Models
+{
+    // Modelo
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+    }
+
+    // Creación de la tabla en la base de datos
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
